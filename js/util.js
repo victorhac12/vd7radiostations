78c1291bd70ecdbc73840840e38ce4ee19fcb0e1
Victor <Victor.ac12@gmail.com>
2022-07-26 15:20:24 -0500

    different browser

--- js/util.js
+++ js/util.js
@@ -45,14 +45,14 @@ function createVideo(stationValues){
   video.src = stationValues.getAttribute("stream");
   video.setAttribute('type',stationValues.getAttribute("streamtype"));
   video.setAttribute('id', stationValues.getAttribute("id"));
-  video.volume = 0.8;
+  video.volume = 1;
   console.log(stationValues.getAttribute("stream"));
   console.log(stationValues.getAttribute("streamtype"));
   allowPlay = false;
   playVideo();
  
  }
- 
+
 }
 function playVideo(){
   video.play();
@@ -62,5 +62,152 @@ function playVideo(){
 function pauseVideo(){
  video.pause();
  allowPlay = true;
+
+}
+
+/* no used
+let BrowserDetect = {
+	init: function () {
+		this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+		this.version = this.searchVersion(navigator.userAgent)
+			//|| this.searchVersion(navigator..appVersion)
+			|| "an unknown version";
+		this.OS = this.searchString(this.dataOS) || "an unknown OS";
+	},
+	searchString: function (data) {
+		for (var i=0;i<data.length;i++)	{
+			var dataString = data[i].string;
+			var dataProp = data[i].prop;
+			this.versionSearchString = data[i].versionSearch || data[i].identity;
+			if (dataString) {
+				if (dataString.indexOf(data[i].subString) != -1)
+					return data[i].identity;
+			}
+			else if (dataProp)
+				return data[i].identity;
+		}
+	},
+	searchVersion: function (dataString) {
+		var index = dataString.indexOf(this.versionSearchString);
+		if (index == -1) return;
+		return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+	},
+	dataBrowser: [
+		{
+			string: navigator.userAgent,
+			subString: "Chrome",
+			identity: "Chrome"
+		},
+		{ 	string: navigator.userAgent,
+			subString: "OmniWeb",
+			versionSearch: "OmniWeb/",
+			identity: "OmniWeb"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Apple",
+			identity: "Safari",
+			versionSearch: "Version"
+		},
+		{
+			prop: window.opera,
+			identity: "Opera",
+			versionSearch: "Version"
+		},
+		{
+			string: navigator.vendor,
+			subString: "iCab",
+			identity: "iCab"
+		},
+		{
+			string: navigator.vendor,
+			subString: "KDE",
+			identity: "Konqueror"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Firefox",
+			identity: "Firefox"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Camino",
+			identity: "Camino"
+		},
+		{		// for newer Netscapes (6+)
+			string: navigator.userAgent,
+			subString: "Netscape",
+			identity: "Netscape"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "MSIE",
+			identity: "Explorer",
+			versionSearch: "MSIE"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Gecko",
+			identity: "Mozilla",
+			versionSearch: "rv"
+		},
+		{ 		// for older Netscapes (4-)
+			string: navigator.userAgent,
+			subString: "Mozilla",
+			identity: "Netscape",
+			versionSearch: "Mozilla"
+		}
+	],
+	dataOS : [
+		{
+			string: navigator.platform,
+			subString: "Win",
+			identity: "Windows"
+		},
+		{
+			string: navigator.platform,
+			subString: "Mac",
+			identity: "Mac"
+		},
+		{
+			   string: navigator.userAgent,
+			   subString: "iPhone",
+			   identity: "iPhone/iPod"
+	    },
+		{
+			string: navigator.platform,
+			subString: "Linux",
+			identity: "Linux"
+		}
+	]
+
+};
+*/
+function loadVersionOS(){
+//BrowserDetect.init();
+//document.write('<p class="accent">You\'re using ' + BrowserDetect.browser + ' ' + BrowserDetect.version + ' on ' + BrowserDetect.OS + '! <img src="images/' + BrowserDetect.browser + '.gif"></p>');
+let userAgent = navigator.userAgent;
+let browVersion;
+console.log(userAgent);
+if (userAgent.includes('Firefox/')) {
+  console.log(`Firefox v${userAgent.split('Firefox/')[1]}`)
+  window.location.href = "/newbrowradio.html";
+} else if (userAgent.includes('Edg/')) {
+  console.log(`Edg v${userAgent.split('Edg/')[1]}`)
+  window.location.href = "/newbrowradio.html";
+} else if (userAgent.includes('Chrome/')) {
+  console.log(`Chrome v${userAgent.split('Chrome/')[1]}`)
+  window.location.href = "/newbrowradio.html";
+} else if (userAgent.includes('Safari/')) {
+  // Safari
+  browVersion = userAgent.split('Safari/')[1];
+  console.log(browVersion);
+  if (browVersion.includes("7.")){
+    window.location.href = "/oldbrowradio.html";
+  }else{
+    window.location.href = "/newbrowradio.html";
+  }
+
+}
 
 }
